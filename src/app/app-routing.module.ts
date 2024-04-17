511e677930a474ebc7f564aa450236c8901aa396
JanCleid <jancel0703@gmail.com>
2024-04-17 14:07:15 +0800

    UI update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './features/action/category-list/category-list.component';
+import { AddCategoryComponent } from './features/action/add-category/add-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/categories', component: CategoryListComponent
+  },
+  {
+    path: 'admin/categories/add', component: AddCategoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
